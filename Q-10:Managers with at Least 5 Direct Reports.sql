766d5e377bf546b8b49bf23ab2bd0693997c6306
Omar Faruk <91719247+farubroh@users.noreply.github.com>
2025-04-27 15:31:52 +0600

    Update Q-10:Managers with at Least 5 Direct Reports.sql

--- Q-10:Managers with at Least 5 Direct Reports.sql
+++ Q-10:Managers with at Least 5 Direct Reports.sql
@@ -1,4 +1,4 @@
--- Q-XX: Managers with at Least 5 Direct Reports
+-- Q-10: Managers with at Least 5 Direct Reports
 -- Problem Link: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/description/?envType=study-plan-v2&envId=top-sql-50>
 
 -- Summary:
